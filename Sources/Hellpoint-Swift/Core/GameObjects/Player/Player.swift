40eb2d2994739c823bcb0380b4fe06b82d650749
Conifer Coniferoslav <64596327+conifer-dev@users.noreply.github.com>
2022-02-28 21:21:23 +0000

    Player rotation based on mouse position.

--- Sources/Hellpoint-Swift/Core/GameObjects/Player/Player.swift
+++ Sources/Hellpoint-Swift/Core/GameObjects/Player/Player.swift
@@ -3,8 +3,10 @@ import Aeni
 
 struct Player : GameObject {
     var velocity: Vector2 = Vector2(x: 0, y: 0)
-    var movementSpeed: Float = 200
+    var movementSpeed: Float = 250
     var sprite: Sprite = Sprite(spriteSheet: Resources.manager.loadedResourcesDatabase["playerSpriteSheet"]!, frameDimensions: Vector2(x: 24, y:24), scale: Vector2(x:2, y:2), position: Vector2(x: 50, y: 50))
+    var isMoving: Bool = false
+
     // Animations
     lazy var idle: SpriteAnimator = SpriteAnimator(sprite: sprite, origin: Vector2(x: 0, y: 3), rotation: 0, startingFrame: 0, endingFrame: 4, column: 0, duration: 0, animationSpeed: 0.15, repeatable: true, tintColor: .white, debugMode: true)
     lazy var running: SpriteAnimator = SpriteAnimator(sprite: sprite, origin: Vector2(x: 0, y: 3), rotation: 0, startingFrame: 0, endingFrame: 6, column: 1, duration: 0, animationSpeed: 0.11, repeatable: true, tintColor: .white, debugMode: true)
@@ -21,17 +23,21 @@ struct Player : GameObject {
     }
 
     mutating func playerMovement(deltaTime dt: Float) {
+        self.isMoving = false
         self.velocity.x = 0
         self.velocity.y = 0
+
 
         if Raylib.isKeyDown(.letterD) {
             self.velocity.x += 1
             self.animation.flipSprite(horizontal: false, vertical: false)
+            self.isMoving = true
         }
 
         if Raylib.isKeyDown(.letterA) {
             self.velocity.x -= 1
             self.animation.flipSprite(horizontal: true, vertical: false)
+            self.isMoving = true
         }
 
         if Raylib.isKeyDown(.letterW) {
@@ -45,15 +51,20 @@ struct Player : GameObject {
         self.velocity = self.velocity.normalized()
         self.sprite.position.x += self.velocity.x * self.movementSpeed * dt
         self.sprite.position.y += self.velocity.y * self.movementSpeed * dt
+
     }
 
     mutating func rotatePlayerOnMousePos() {
         let mousePosX = Raylib.getMouseX()
 
-        if Float(mousePosX) > self.sprite.position.x {
-            self.animation.flipSprite(horizontal: false, vertical: false)
+        if !self.isMoving {
+            if Float(mousePosX) > self.sprite.position.x {
+                self.animation.flipSprite(horizontal: false, vertical: false)
+            } else {
+                self.animation.flipSprite(horizontal: true, vertical: false)
+            }
         } else {
-            self.animation.flipSprite(horizontal: true, vertical: false)
+            return
         }
     }
 }
